4b53ee5b930ed4cb351a3ae4538affd9b4a08ba8
GoldFrosch <goldfrosch@daum.net>
2025-02-07 17:15:45 +0900

    feat: Ability System Component 연동

---
+++ Source/AxeOfSword/SM/GAS/AOSAbilitySystemComponent.cpp
@@ -0,0 +1,35 @@
+﻿#include "AOSAbilitySystemComponent.h"
+#include "AxeOfSword/SM/GAS/Ability/BaseInputAbility.h"
+#include "AOSAbilitySystemInitializeData.h"
+
+UAOSAbilitySystemComponent::UAOSAbilitySystemComponent()
+{
+	
+}
+
+void UAOSAbilitySystemComponent::Initialize()
+{
+	if (GetIsInitialize())
+	{
+		return;
+	}
+
+	if (!InitialData->GetDefaultGameplayAbilities().IsEmpty())
+	{
+		for (TSubclassOf<UBaseInputAbility> Ability : InitialData->GetDefaultGameplayAbilities())
+		{
+			UBaseInputAbility* InputAbility = Ability->GetDefaultObject<UBaseInputAbility>();
+
+			GiveAbility(FGameplayAbilitySpec(
+				Ability, InputAbility->GetAbilityLevel()
+				, static_cast<uint32>(InputAbility->GetInputId()), this));
+		}
+	}
+
+	if (!InitialData->GetDefaultGameplayTags().IsEmpty())
+	{
+		AddLooseGameplayTags(InitialData->GetDefaultGameplayTags());
+	}
+	
+	SetIsInitialize(true);
+}
