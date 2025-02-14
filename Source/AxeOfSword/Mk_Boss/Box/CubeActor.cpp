db5542ed485d8dd1b4ba6ccc5c9b38cbfe7b5733
smk6931 <smk6931@naver.com>
2025-02-14 14:35:29 +0900

    상민이의 SKm_Manny Socket 수정 도끼 장착

--- Source/AxeOfSword/Mk_Boss/Box/CubeActor.cpp
+++ Source/AxeOfSword/Mk_Boss/Box/CubeActor.cpp
@@ -30,12 +30,18 @@ ACubeActor::ACubeActor()
 void ACubeActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Root->OnComponentBeginOverlap.AddDynamic(this,&ACubeActor::OnMyBeginOverlap);
 }
 
 // Called every frame
 void ACubeActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void ACubeActor::OnMyBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
 }
 
--- Source/AxeOfSword/Mk_Boss/Box/CubeActor.h
+++ Source/AxeOfSword/Mk_Boss/Box/CubeActor.h
@@ -28,4 +28,9 @@ public:
 
 	UPROPERTY(EditAnywhere,BlueprintReadWrite)
 	class UStaticMeshComponent* Box;
+
+	UFUNCTION()
+	void OnMyBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+		const FHitResult& SweepResult);
 };
