4f99bec0c0f96d4cd15e7ca089970f9d7c945821
GoldFrosch <goldfrosch@daum.net>
2025-02-14 20:06:55 +0900

    feat: zoom in, out 관련 cue 생성

---
+++ Source/AxeOfSword/SM/GAS/Ability/Active/Player/Hold/GA_CloseHold.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AxeOfSword/SM/GAS/Ability/Utility/BaseInputAbility.h"
+#include "GA_CloseHold.generated.h"
+
+UCLASS()
+class AXEOFSWORD_API UGA_CloseHold : public UBaseInputAbility
+{
+	GENERATED_BODY()
+};
--- Source/AxeOfSword/SM/GAS/Cue/GC_CameraMove.cpp
+++ Source/AxeOfSword/SM/GAS/Cue/GC_CameraMove.cpp
@@ -7,18 +7,32 @@ AGC_CameraMove::AGC_CameraMove()
 {
 	SetHidden(true);
 	CameraMoveTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("Camera Move Timeline Component"));
+	FovMoveTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("Fov Move Timeline Component"));
 }
 
 void AGC_CameraMove::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (IsEnabledCameraMove)
+	{
+		CameraMoveCallback.BindDynamic(this, &ThisClass::OnCameraMoveCallback);
+		CameraMoveFinish.BindDynamic(this, &ThisClass::OnCameraMoveFinish);
 	
-	CameraMoveCallback.BindDynamic(this, &ThisClass::OnCameraMoveCallback);
-	CameraMoveFinish.BindDynamic(this, &ThisClass::OnCameraMoveFinish);
+		CameraMoveTimeline->SetLooping(false);
+		CameraMoveTimeline->AddInterpVector(CameraMoveTimingCurve, CameraMoveCallback);
+		CameraMoveTimeline->SetTimelineFinishedFunc(CameraMoveFinish);
+	}
+
+	if (IsEnabledCameraFov)
+	{
+		FovMoveCallback.BindDynamic(this, &ThisClass::AGC_CameraMove::OnFovMoveCallback);
+		FovMoveFinish.BindDynamic(this, &ThisClass::AGC_CameraMove::OnFovMoveFinish);
 	
-	CameraMoveTimeline->SetLooping(false);
-	CameraMoveTimeline->AddInterpVector(CameraMoveTimingCurve, CameraMoveCallback);
-	CameraMoveTimeline->SetTimelineFinishedFunc(CameraMoveFinish);
+		FovMoveTimeline->SetLooping(false);
+		FovMoveTimeline->AddInterpFloat(FovMoveTimingCurve, FovMoveCallback);
+		FovMoveTimeline->SetTimelineFinishedFunc(FovMoveFinish);
+	}
 }
 
 bool AGC_CameraMove::OnExecute_Implementation(AActor* MyTarget, const FGameplayCueParameters& Parameters)
@@ -37,7 +51,7 @@ bool AGC_CameraMove::OnExecute_Implementation(AActor* MyTarget, const FGameplayC
 
 void AGC_CameraMove::OnCameraMoveCallback(FVector Output)
 {
-	APlayerCharacter* Player = Cast<APlayerCharacter>(Target);
+	const APlayerCharacter* Player = Cast<APlayerCharacter>(Target);
 	if (!Player)
 	{
 		return;
@@ -51,11 +65,43 @@ void AGC_CameraMove::OnCameraMoveCallback(FVector Output)
  
 void AGC_CameraMove::OnCameraMoveFinish()
 {
-	APlayerCharacter* Player = Cast<APlayerCharacter>(Target);
+	const APlayerCharacter* Player = Cast<APlayerCharacter>(Target);
 	if (!Player)
 	{
 		return;
 	}
 	
-	Player->GetCameraComponent()->RollbackToFirstCameraOption();
+	if (IsRollback)
+	{
+		Player->GetCameraComponent()->RollbackToFirstCameraOption();
+	}
+}
+
+void AGC_CameraMove::OnFovMoveCallback(float Output)
+{
+	const APlayerCharacter* Player = Cast<APlayerCharacter>(Target);
+	if (!Player)
+	{
+		return;
+	}
+
+	UPlayerCameraComponent* PlayerCameraComponent = Player->GetCameraComponent();
+	
+	PlayerCameraComponent->SetCameraOption(
+		PlayerCameraComponent->GetRelativeLocation(),
+		PlayerCameraComponent->GetDefaultFov() + Output * CameraFovSpeed);
+}
+
+void AGC_CameraMove::OnFovMoveFinish()
+{
+	const APlayerCharacter* Player = Cast<APlayerCharacter>(Target);
+	if (!Player)
+	{
+		return;
+	}
+
+	if (IsRollback)
+	{
+		Player->GetCameraComponent()->RollbackToFirstCameraOption();
+	}
 }
