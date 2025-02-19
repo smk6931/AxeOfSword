3827d38181f171664ebea9dd1f3dffe89032f618
smk6931 <smk6931@naver.com>
2025-02-19 11:04:44 +0900

    큐브 올리기

---
+++ Source/AxeOfSword/Mk_Boss/TestCube/TestCube.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestCube.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+ATestCube::ATestCube()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<UBoxComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Root);
+
+	Root->SetGenerateOverlapEvents(true);
+}
+
+// Called when the game starts or when spawned
+void ATestCube::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Root->OnComponentBeginOverlap.AddDynamic(this,&ATestCube::OnMyBeginOverlap);
+}
+
+// Called every frame
+void ATestCube::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATestCube::OnMyBeginOverlap(UPrimitiveComponent* OverlappedCompneComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
