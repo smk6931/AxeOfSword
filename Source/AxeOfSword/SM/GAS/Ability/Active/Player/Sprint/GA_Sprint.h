5283acde7c3588e64f0679c63d216ba20cc95d42
GoldFrosch <goldfrosch@daum.net>
2025-02-21 14:12:53 +0900

    feat: 도끼 복귀시에 대한 처리

--- Source/AxeOfSword/SM/GAS/Ability/Active/Player/Sprint/GA_Sprint.h
+++ Source/AxeOfSword/SM/GAS/Ability/Active/Player/Sprint/GA_Sprint.h
@@ -10,6 +10,10 @@ class AXEOFSWORD_API UGA_Sprint : public UBaseInputAbility
 	GENERATED_BODY()
 
 protected:
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr,
+		const FGameplayTagContainer* TargetTags = nullptr, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+	
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 		const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
 		const FGameplayEventData* TriggerEventData) override;
